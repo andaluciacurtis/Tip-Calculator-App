bf1945a01a6feb36159998e1c436cda88cf1004d
andaluciacurtis <andalucia.curtis@gmail.com>
2023-10-30 15:23:22 -0700

    Wrote pseudocode

---
+++ script.js
@@ -0,0 +1,34 @@
+const resetButton = document.querySelector(".reset");
+const tipButtons = document.querySelectorAll(".button-grid > div");
+
+const tipAmount = document.querySelector(".tip-amount");
+const totalPerPerson = document.querySelector(".total-per-person");
+
+// RESET
+resetButton.addEventListener("click", ()=>{
+  // Reset input
+  // Deselect tip
+  // Get rid of error messages
+
+  // Reset tip and total amount
+});
+
+// BILL
+// Check to see if bill is a correct number
+// Save into variable
+
+// TIP
+// Check to see if one button is already selected
+// If so: remove selected from that one
+//        add selected to new button
+//        save amount in variable
+
+// NUMBER OF PEOPLE
+// Check to see if number of people is a valid number
+// Save into variable
+
+// TIP AMOUNT
+// display bill/person * tip %
+
+// TOTAL 
+// display bill + tip %
